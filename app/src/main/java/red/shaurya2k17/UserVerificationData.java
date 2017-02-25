d34ab612a3e11e117f896096446ed5a8787cc698
anr007 <narayanareddy500@gmail.com>
2017-02-25 17:44:05 +0530

    Added Security using IMEI verification

---
+++ app/src/main/java/red/shaurya2k17/UserVerificationData.java
@@ -0,0 +1,36 @@
+package red.shaurya2k17;
+
+/**
+ * Created by reddy on 25/2/17.
+ */
+
+public class UserVerificationData {
+
+    String mEmail;
+    String mUid;
+    String mDeviceId;
+
+    public String getmEmail() {
+        return mEmail;
+    }
+
+    public void setmEmail(String mEmail) {
+        this.mEmail = mEmail;
+    }
+
+    public String getmUid() {
+        return mUid;
+    }
+
+    public void setmUid(String mUid) {
+        this.mUid = mUid;
+    }
+
+    public String getmDeviceId() {
+        return mDeviceId;
+    }
+
+    public void setmDeviceId(String mDeviceId) {
+        this.mDeviceId = mDeviceId;
+    }
+}
